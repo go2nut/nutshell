b1d947ed1e8620552d0d8539489b2f56cfe3b6b7
孙微 <hisunwei@outlook.com>
2021-05-07 19:16:26 +0800

    merge

--- nutlet/proxy/server.go
+++ nutlet/proxy/server.go
@@ -20,7 +20,8 @@ func ServeAppProxy(httpPort, grpcPort int) *AppRouter {
 }
 
 func ServeDns() {
-	p := exec.Command("sed", "-i", "1s/^/nameserver 127.0.0.1\n/", "/etc/resolv.conf")
+	//p := exec.Command("sed", "-i", "1s/^/nameserver 127.0.0.1\n", "/etc/resolv.conf")
+	p := exec.Command("echo 'nameserver 127.0.0.1' >> /etc/resolv.conf2 && cat /etc/resolv.conf >> /etc/resolv.conf2 && mv /etc/resolv.conf2 /etc/resolv.conf")
 	err := p.Run()
 	log.Infof("edit nameserver command, cmd:%s err:%v", p.String(), err)
 	protocols.ServeDns(53, map[string]string{"*.nutshell": "127.0.0.1"})
